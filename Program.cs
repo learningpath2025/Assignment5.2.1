6817e783a9e38d60f387aa32e68cbaa287bf0dd5
learningpath2025 <learningpath2025@outlook.com>
2025-09-22 11:36:30 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment5._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLastWordLength(GetInputs()));
+        }
+
+        public static string GetInputs()
+        {
+            string input;
+            Console.Write($"Enter a string: ");
+            input = Console.ReadLine() ?? "";
+            return input;
+        }
+
+        public static int GetLastWordLength(string s)
+        {
+            string seperator = " ";
+            string[] ws = s.Split(seperator);
+            string lw = ws[ws.Length - 1];
+            return lw.Length;
+        }
+    }
+}
